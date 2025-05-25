8a3ddce0e6297dbb7af3b175b6e1d17a89b5ac81
tcpasf <alphaalberto79@gmail.com>
2025-05-25 16:44:34 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Alpha-Works'
+description 'QBCore Vehicle Auto-Delete System'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+dependencies {
+    'qb-core'
+}
